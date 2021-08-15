bfc77accb850b2cac1649b665233a7d4b7287a94
chLi <565353780@qq.com>
2021-08-15 19:37:14 +0800

    now can auto load all deeplab model

--- DeepLabV3PlusDetector.py
+++ DeepLabV3PlusDetector.py
@@ -1,6 +1,7 @@
 import os
 import utils
 import network
+import time
 
 from PIL import Image
 import torch
@@ -12,6 +13,8 @@ from datasets import VOCSegmentation, Cityscapes, cityscapes
 class DeepLabV3PlusDetector:
     def __init__(self):
         self.reset()
+
+        os.environ['CUDA_VISIBLE_DEVICES'] = self.gpu_id
         return
 
     def reset(self):
@@ -23,8 +26,17 @@ class DeepLabV3PlusDetector:
             'deeplabv3_mobilenet': network.deeplabv3_mobilenet,
             'deeplabv3plus_mobilenet': network.deeplabv3plus_mobilenet
         }
-        self.valid_dataset = ['voc', 'cityscapes']
+        self.valid_model_name_list = ['deeplabv3_resnet50',
+                                      'deeplabv3plus_resnet50',
+                                      'deeplabv3_resnet101',
+                                      'deeplabv3plus_resnet101',
+                                      'deeplabv3_mobilenet',
+                                      'deeplabv3plus_mobilenet']
+        self.valid_backend_name_list = ['resnet50', 'resnet101', 'mobilenet']
+        self.valid_dataset_name_list = ['voc', 'cityscapes']
+        self.model_name = None
         self.checkpoint = None
+        self.checkpoint_ready = False
         self.dataset = None
         self.model = None
         self.model_ready = False
@@ -38,7 +50,6 @@ class DeepLabV3PlusDetector:
         self.gpu_id = '0'
         self.crop_val = False
         self.crop_size = 513
-        self.save_val_results_to = "./result"
         self.val_batch_size = 4
         self.num_classes = None
         self.decode_fn = None
@@ -81,8 +92,48 @@ class DeepLabV3PlusDetector:
 
         return int(1.0 * self.detected_num / self.total_time_sum)
 
-    def setDataset(self, dataset):
-        self.dataset = dataset
+    def setCheckPoint(self, checkpoint):
+        self.checkpoint = checkpoint
+        self.checkpoint_ready = False
+
+        checkpoint_file_name = os.path.basename(checkpoint)
+
+        is_plus = False
+        backend_index = -1
+        dataset_index = -1
+
+        for i in range(len(self.valid_backend_name_list)):
+            if self.valid_backend_name_list[i] in checkpoint_file_name:
+                backend_index = i
+                break
+
+        for i in range(len(self.valid_dataset_name_list)):
+            if self.valid_dataset_name_list[i] in checkpoint_file_name:
+                dataset_index = i
+                break
+
+        if 'deeplabv3' not in checkpoint_file_name:
+            print("Cannot load this checkpoint type!")
+            return
+
+        if 'plus' in checkpoint_file_name:
+            is_plus = True
+
+        if backend_index == -1 or dataset_index == -1:
+            print("Cannot load this checkpoint type!")
+            return
+
+        self.model_name = 'deeplabv3'
+        if is_plus:
+            self.model_name += 'plus'
+        self.model_name += '_'
+        self.model_name += self.valid_backend_name_list[backend_index]
+        self.dataset = self.valid_dataset_name_list[dataset_index]
+
+        self.checkpoint_ready = True
+        return
+
+    def setDataset(self):
         if self.dataset == 'voc':
             self.num_classes = 21
             self.decode_fn = VOCSegmentation.decode_target
@@ -91,16 +142,23 @@ class DeepLabV3PlusDetector:
             self.decode_fn = Cityscapes.decode_target
         return
 
-    def loadModel(self, checkpoint, dataset):
+    def initEnv(self, checkpoint):
         self.reset()
 
-        self.checkpoint = checkpoint
+        self.setCheckPoint(checkpoint)
+        if not self.checkpoint_ready:
+            return
 
-        self.setDataset(dataset)
+        self.setDataset()
+        return
 
-        os.environ['CUDA_VISIBLE_DEVICES'] = self.gpu_id
+    def loadModel(self, checkpoint):
+        self.initEnv(checkpoint)
 
-        self.model = self.model_map["deeplabv3plus_resnet50"](num_classes=self.num_classes, output_stride=self.output_stride)
+        if not self.checkpoint_ready:
+            return
+
+        self.model = self.model_map[self.model_name](num_classes=self.num_classes, output_stride=self.output_stride)
 
         if self.separable_conv and 'plus' in self.model:
             network.convert_to_separable_conv(self.model.classifier)
@@ -112,6 +170,8 @@ class DeepLabV3PlusDetector:
             self.model = nn.DataParallel(self.model)
             self.model.to(self.device)
             print("Resume model from %s" % self.checkpoint)
+            print("Model loaded : ", self.model_name)
+            print("Dataset loaded : ", self.dataset)
             del checkpoint_data
         else:
             print("[!] Retrain")
@@ -119,6 +179,7 @@ class DeepLabV3PlusDetector:
             self.model.to(self.device)
 
         #denorm = utils.Denormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # denormalization for ori images
+
         if self.crop_val:
             self.transform = T.Compose([
                 T.Resize(crop_size),
@@ -134,6 +195,7 @@ class DeepLabV3PlusDetector:
                 ])
 
         self.model = self.model.eval()
+        self.model_ready = True
         return
 
     def detect(self, image):
@@ -142,32 +204,102 @@ class DeepLabV3PlusDetector:
             image = image.to(self.device)
             result = self.model(image).max(1)[1].cpu().numpy()[0] # HW
         return result
+
+    def test(self, image_folder_path, run_episode=10, timer_skip_num=5):
+        if not self.model_ready:
+            print("Model not ready yet, Please loadModel or check your model path first!")
+            return
+
+        if run_episode == 0:
+            print("No detect run with run_episode=0!")
+            return
  
+        file_name_list = os.listdir(image_folder_path)
+        image_file_name_list = []
+        for file_name in file_name_list:
+            if file_name[-4:] in [".jpg", ".png"]:
+                image_file_name_list.append(file_name)
+
+        if run_episode < 0:
+            self.resetTimer()
+            timer_skipped_num = 0
+
+            while True:
+                for image_file_name in image_file_name_list:
+                    image_file_path = os.path.join(image_folder_path, image_file_name)
+
+                    self.startTimer()
+
+                    image_file_path = os.path.join(image_folder_path, image_file_name)
+                    image = Image.open(image_file_path).convert('RGB')
+
+                    result = self.detect(image)
+
+                    colorized_result = deeplabv3plus_detector.decode_fn(result).astype('uint8')
+                    colorized_result = Image.fromarray(colorized_result)
+
+                    colorized_result.save(os.path.join("./result/", image_file_name.split('.')[0] +'.png'))
+
+                    if timer_skipped_num < timer_skip_num:
+                        self.endTimer(False)
+                        timer_skipped_num += 1
+                    else:
+                        self.endTimer()
+
+                    print("\rNet: " + self.model_name +
+                          "\tDetected: " + str(self.detected_num) +
+                          "\tAvgTime: " + str(self.getAverageTime()) +
+                          "\tAvgFPS: " + str(self.getAverageFPS()) +
+                          "    ", end="")
+
+            print()
+
+            return
+
+        self.resetTimer()
+        total_num = run_episode * len(image_file_name_list)
+        timer_skipped_num = 0
+
+        for i in range(run_episode):
+            for image_file_name in image_file_name_list:
+                image_file_path = os.path.join(image_folder_path, image_file_name)
+
+                self.startTimer()
+
+                image_file_path = os.path.join(image_folder_path, image_file_name)
+                image = Image.open(image_file_path).convert('RGB')
+
+                result = self.detect(image)
+
+                colorized_result = deeplabv3plus_detector.decode_fn(result).astype('uint8')
+                colorized_result = Image.fromarray(colorized_result)
+
+                colorized_result.save(os.path.join("./result/", image_file_name.split('.')[0] +'.png'))
+
+                if timer_skipped_num < timer_skip_num:
+                    self.endTimer(False)
+                    timer_skipped_num += 1
+                else:
+                    self.endTimer()
+
+                print("\rNet: " + self.model_name +
+                      "\tDetected: " + str(self.detected_num) + "/" + str(total_num - timer_skip_num) +
+                      "\t\tAvgTime: " + str(self.getAverageTime()) +
+                      "\tAvgFPS: " + str(self.getAverageFPS()) +
+                      "    ", end="")
+
+        print()
+
+
 if __name__ == '__main__':
     image_folder_path = "./samples/"
     checkpoint = "./best_deeplabv3plus_resnet50_voc_os16.pth"
-    dataset = 'voc'
-    save_val_results_to = "./result/"
-    if save_val_results_to is not None:
-        os.makedirs(save_val_results_to, exist_ok=True)
+
+    os.makedirs("./result/", exist_ok=True)
 
     deeplabv3plus_detector = DeepLabV3PlusDetector()
 
-    deeplabv3plus_detector.loadModel(checkpoint, dataset)
+    deeplabv3plus_detector.loadModel(checkpoint)
 
-    image_file_name_list = os.listdir(image_folder_path)
-
-    for image_file_name in image_file_name_list:
-        if image_file_name[-4:] not in [".jpg", ".png"]:
-            continue
-        image_file_path = os.path.join(image_folder_path, image_file_name)
-        image = Image.open(image_file_path).convert('RGB')
-
-        result = deeplabv3plus_detector.detect(image)
-
-        colorized_result = deeplabv3plus_detector.decode_fn(result).astype('uint8')
-        colorized_result = Image.fromarray(colorized_result)
-
-        if save_val_results_to:
-            colorized_result.save(os.path.join(save_val_results_to, image_file_name.split('.')[0] +'.png'))
+    deeplabv3plus_detector.test(image_folder_path)
 
